8bcb559c680ac7d1f36b7f6491d699649981df77
ostro <ostr00000@gmail.com>
2020-08-20 23:26:27 +0200

    improve meta wrapper

--- lib/pyqt_settings/metaclass/base.py
+++ lib/pyqt_settings/metaclass/base.py
@@ -4,7 +4,11 @@ from typing import Type
 class BaseMeta(type):
     @classmethod
     def wrap(mcs, otherClass: Type):
-        class Wrapper(mcs, type(otherClass)):
+        return mcs.wrapMetaClass(type(otherClass))
+
+    @classmethod
+    def wrapMetaClass(mcs, otherMetaClass):
+        class Wrapper(mcs, otherMetaClass):
             pass
 
         return Wrapper
