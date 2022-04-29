e715ea4691bc1cd0e7b096598bc9a00ecedd9bb8
ostr00000 <ostr00000@gmail.com>
2022-04-29 22:41:09 +0200

    fix missing icon

--- lib/pyqt_settings/action.py
+++ lib/pyqt_settings/action.py
@@ -2,12 +2,13 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QDialog
 from boltons.cacheutils import cachedproperty
 
+from pyqt_settings import packageName
 from pyqt_settings.dialog import createSettingDialogClass
 from pyqt_utils.qobjects.display_widget_action import DisplayWidgetAction
 
 
 class SettingDialogAction(DisplayWidgetAction):
-    def __init__(self, settings, icon=QIcon(':/icons/settings.svg'),
+    def __init__(self, settings, icon=QIcon(f'{packageName}:settings.svg'),
                  text="&Settings", parent=None):
         super().__init__(icon=icon, text=text, parent=parent)
         self.settings = settings
